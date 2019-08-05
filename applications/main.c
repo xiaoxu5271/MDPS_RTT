1134a83f5c93e57b5eee12371c91deebfa0288cd
xiaoxu5271 <liuchunxun@qq.com>
2019-08-05 15:02:45 +0800

    2019-08-05 15:00:47

---
+++ applications/main.c
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2006-2018, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2018-11-06     SummerGift   first version
+ * 2018-11-19     flybreak     add stm32f407-atk-explorer bsp
+ */
+
+#include <rtthread.h>
+#include <rtdevice.h>
+#include <board.h>
+
+#include "fal.h"
+
+/* defined the LED0 pin: PF9 */
+#define LED0_PIN GET_PIN(F, 0)
+#define LED1_PIN GET_PIN(F, 1)
+#define LED2_PIN GET_PIN(F, 2)
+
+#define APP_VERSION "V0.0.3"
+
+int main(void)
+{
+    int count = 1;
+    /* set LED0 pin mode to output */
+    rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);
+    rt_pin_mode(LED1_PIN, PIN_MODE_OUTPUT);
+    rt_pin_mode(LED2_PIN, PIN_MODE_OUTPUT);
+
+    fal_init();
+    rt_kprintf("The current version of APP firmware is %s\n", APP_VERSION);
+
+    while (count++)
+    {
+        rt_pin_write(LED0_PIN, PIN_LOW);
+        rt_pin_write(LED1_PIN, PIN_LOW);
+        rt_pin_write(LED2_PIN, PIN_LOW);
+        rt_thread_mdelay(50);
+        rt_pin_write(LED0_PIN, PIN_HIGH);
+        rt_pin_write(LED1_PIN, PIN_HIGH);
+        rt_pin_write(LED2_PIN, PIN_HIGH);
+        rt_thread_mdelay(1000);
+    }
+
+    return RT_EOK;
+}
+
+/**
+ * Function    ota_app_vtor_reconfig
+ * Description Set Vector Table base location to the start addr of app(RT_APP_PART_ADDR).
+*/
+static int ota_app_vtor_reconfig(void)
+{
+#define NVIC_VTOR_MASK 0x3FFFFF80
+    /* Set the Vector Table base location by user application firmware definition */
+    SCB->VTOR = RT_APP_PART_ADDR & NVIC_VTOR_MASK;
+
+    return 0;
+}
+INIT_BOARD_EXPORT(ota_app_vtor_reconfig);
